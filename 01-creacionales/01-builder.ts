a17a1985b985313d52a5dc5afc58c9ff1397814a
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-20 17:08:10 -0600

    feat: Ejecicio 1 - Ejemplo del patrón Builder

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,74 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+  public cpu: string = "CPU - not defined";
+  public ram: string = "RAM - not defined";
+  public storage: string = "Storage - not defined";
+  public gpu?: string;
+
+  displayingConfiguration() {
+    console.log(`Configuración de la computadora
+        CPU: ${this.cpu}
+        RAM: ${this.ram}
+        Almacenamiento: ${this.storage}
+        GPU: ${this.gpu ?? "No tiene GPU"}`);
+  }
+}
+
+class ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCPU(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+    return this;
+  }
+
+  setRAM(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+    return this;
+  }
+
+  setGPU(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+    return this;
+  }
+
+  build() {
+    return this.computer;
+  }
+}
+
+function main() {
+  const basicComputer = new ComputerBuilder()
+    .setCPU("Intel Core 2 Dúo")
+    .setRAM("4GB")
+    .setStorage("256GB")
+    .build();
+
+  console.log("%cComputadora básica: ", COLORS.blue);
+  basicComputer.displayingConfiguration();
+
+  const computer = new ComputerBuilder()
+    .setCPU("Intel i9")
+    .setRAM("32GB")
+    .setStorage("8TB SSD")
+    .setGPU("Nvidia RTX 5070")
+    .build();
+
+  console.log("%cComputadora: ", COLORS.red);
+  computer.displayingConfiguration();
+}
+
+main();
