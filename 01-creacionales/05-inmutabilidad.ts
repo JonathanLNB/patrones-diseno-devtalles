b2d3b4757af41aec159898edbea4bbcb4fcfc463
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-28 13:35:30 -0600

    feat: Ejemplo patrón Inmutabilidad con Copia

---
+++ 01-creacionales/05-inmutabilidad.ts
@@ -0,0 +1,64 @@
+/**
+ * ! Inmutabilidad con copia
+ * Aunque la inmutabilidad es una buena práctica, no siempre es posible.
+ * En estos casos, se puede hacer una copia del objeto y modificar la copia.
+ *
+ *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
+ */
+import {COLORS} from "../helpers/colors.ts";
+
+class CodeEditorState {
+  readonly content: string;
+  readonly cursorPosition: number;
+  readonly unsavedChanges: boolean;
+
+  constructor(
+    content: string,
+    cursorPosition: number,
+    unsavedChanges: boolean,
+  ) {
+    this.content = content;
+    this.cursorPosition = cursorPosition;
+    this.unsavedChanges = unsavedChanges;
+  }
+
+  copyWith(
+    { content, cursorPosition, unsavedChanges }: Partial<CodeEditorState>,
+  ) {
+    return new CodeEditorState(
+      content ?? this.content,
+      cursorPosition ?? this.cursorPosition,
+      unsavedChanges ?? this.unsavedChanges,
+    );
+  }
+
+  displayState() {
+    console.log("\n%cEstado del editor: ", COLORS.green);
+    console.log(`
+        Contenido: ${this.content},
+        Cursor Pos: ${this.cursorPosition},
+        UnsavedChanges: ${this.unsavedChanges},
+    `);
+  }
+}
+
+class CodeEditorHistory {
+  private history: CodeEditorState[] = [];
+  private currentIndex: number = -1;
+
+  save(state: CodeEditorState): void {
+    if (this.currentIndex < this.history.length - 1) {
+      this.history = this.history.slice(0, this.history.length + 1);
+    }
+    this.history.push(state);
+    this.currentIndex++;
+  }
+
+  redo(): CodeEditorState | null {
+    if (this.currentIndex < this.history.length - 1) {
+      this.currentIndex++;
+      return this.history[this.history.length - 1];
+    }
+    return null;
+  }
+}
