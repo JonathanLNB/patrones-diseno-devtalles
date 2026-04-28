83d00e7ec085d6440308d113546cd952394399f5
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-28 12:33:17 -0600

    feat: Abstract Factory

---
+++ 01-creacionales/03-abstract-factory.ts
@@ -0,0 +1,76 @@
+/**
+ * ! Abstract Factory:
+ * Es un patrón de diseño que permite crear familias de objetos relacionados
+ * sin especificar sus clases concretas.
+ *
+ * En lugar de crear objetos individuales directamente,
+ * creamos fábricas que producen un conjunto de objetos relacionados.
+ *
+ * * Es útil cuando necesitas crear objetos que son parte de una familia
+ * * y quieres asegurarte de que estos objetos se complementen entre sí.
+ *
+ * https://refactoring.guru/es/design-patterns/abstract-factory
+ */
+
+import {COLORS} from "../helpers/colors.ts";
+
+/**
+ *  El propósito del Abstract Factory es crear familias de objetos relacionados
+ *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
+ *  de cada uno de esos objetos en el código principal.
+ */
+
+interface Hamburger {
+  prepare(): void;
+}
+
+interface Drink {
+  pour(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+  prepare() {
+    console.log("Preparando hamburguesa de %cPollo", COLORS.yellow);
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare() {
+    console.log("Preparando hamburguesa de %cRes", COLORS.red);
+  }
+}
+
+class Water implements Drink {
+  pour() {
+    console.log("Sirviendo un vaso de %cAgua", COLORS.blue);
+  }
+}
+
+class Soda implements Drink {
+  pour() {
+    console.log("Sirviendo un vaso de %cPepsi", COLORS.violet);
+  }
+}
+
+interface RestaurantFactory {
+  createHamburger(): Hamburger;
+  createDrink(): Drink;
+}
+
+class FastFoodRestaurantFactory implements RestaurantFactory {
+  createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+  createDrink(): Drink {
+    return new Soda();
+  }
+}
+
+class HealthyRestaurantFactory implements RestaurantFactory {
+  createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+  createDrink(): Drink {
+    return new Water();
+  }
+}
