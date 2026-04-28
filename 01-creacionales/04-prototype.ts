09eaace7c235e860b3d1f375112e5bd29edc526f
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-28 13:08:18 -0600

    feat: Ejemplo Prototype

---
+++ 01-creacionales/04-prototype.ts
@@ -0,0 +1,43 @@
+class Document {
+  public title: string;
+  public content: string;
+  public author: string;
+
+  constructor(title: string, content: string, author: string) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+
+  displayInfo() {
+    console.log(`
+    Title: ${this.title}
+    Content: ${this.content}
+    Author: ${this.author}
+    `);
+  }
+
+  clone(): Document {
+    return new Document(this.title, this.content, this.author);
+  }
+}
+
+function main() {
+  const document1 = new Document("Cotización", "500 dólares", "Fernando");
+  console.log({ document1 });
+  document1.displayInfo();
+
+  //Se pierde el tipo de dato al realiza un clon de su información.
+  const document2Error = structuredClone(document1);
+  console.log({ document2Error });
+  //JS permite esto por inferir el tipo de dato pero otros lenguajes de programación podrían tirar un error.
+  document2Error.displayInfo();
+
+  //No pierde el tipo de dato y mantiene la accesibilidad a todas sus propiedades.
+  const document2 = document1.clone();
+  console.log({ document2 });
+  document2.title = "Nueva cotización";
+  document2.displayInfo();
+}
+
+main();
