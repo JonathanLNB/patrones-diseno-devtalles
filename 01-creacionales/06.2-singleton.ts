88dbb7d4593bbc2fc128ae89d5885810169b9132
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-28 19:38:02 -0600

    feat: Segundo ejemplo de Singleton con TypeScript

--- 01-creacionales/06.2-singleton.ts
+++ 01-creacionales/06.2-singleton.ts
@@ -8,13 +8,14 @@
  * * objeto de configuración.
  */
 
+import {COLORS} from "../helpers/colors.ts";
+
 class DatabaseConnection {
   private static instance: DatabaseConnection;
   private connected: boolean = false;
 
   // Constructor privado para evitar instancias directas
   private constructor() {
-    this.connected = false;
   }
 
   // Método estático para obtener la instancia única
@@ -22,6 +23,7 @@ class DatabaseConnection {
     // Completar: implementar el patrón Singleton
     if (!DatabaseConnection.instance) {
       DatabaseConnection.instance = new DatabaseConnection();
+      console.log("\n%cConectado a la base de datos", COLORS.blue);
     }
     return DatabaseConnection.instance;
   }
@@ -29,11 +31,11 @@ class DatabaseConnection {
   // Método para conectar a la base de datos
   public connect(): void {
     if (this.connected) {
-      console.log("Database is already connected");
+      console.log("Database is already connected", COLORS.red);
       return;
     }
     this.connected = true;
-    console.log("Database connected");
+    console.log("Database connected", COLORS.green);
     // Completar: si no está conectado, mostrar mensaje de conexión
   }
 
@@ -41,10 +43,10 @@ class DatabaseConnection {
   public disconnect(): void {
     if (this.connected) {
       this.connected = false;
-      console.log("Database disconnected");
+      console.log("Database disconnected", COLORS.green);
       return;
     }
-    console.log("Database is already disconnected");
+    console.log("Database is already disconnected", COLORS.red);
     // Completar: desconectar y mostrar mensaje de desconexión
   }
 }
--- 01-creacionales/06.2.1-singleton.ts
+++ 01-creacionales/06.2.1-singleton.ts
@@ -9,3 +9,14 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+import {configManager} from "./singleton/config-manager.ts";
+import {COLORS} from "../helpers/colors.ts";
+
+configManager.setConfig("apiURL", "http://localhost:8080");
+configManager.setConfig("timeout", "5000");
+configManager.setConfig("apiKey", "123ABC");
+
+console.log(`%c${configManager.getConfig("apiURL")}`, COLORS.green);
+console.log(`%c${configManager.getConfig("timeout")}`, COLORS.green);
+console.log(`%c${configManager.getConfig("apiKey")}`, COLORS.green);
