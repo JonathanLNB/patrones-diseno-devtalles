0858534076f3e5552091d560e9faefe2630c8b67
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-29 18:42:50 -0600

    feat: Ejemplo del patrón de adapter

---
+++ 02-estructurales/adapter-files/logger-adapter.ts
@@ -0,0 +1,32 @@
+import {Logger} from "jsr:@deno-library/logger";
+import {COLORS} from "../../helpers/colors.ts";
+
+interface ILoggerAdapter {
+  file: string;
+
+  writeLog: (msg: string) => void;
+  writeError: (msg: string) => void;
+  writeWarning: (msg: string) => void;
+}
+
+export class DenoLoggerAdapter implements ILoggerAdapter {
+  public file: string;
+  private logger = new Logger();
+
+  constructor(file: string) {
+    this.file = file;
+  }
+
+  writeLog(msg: string) {
+    this.logger.info(`[${this.file} Log] ${msg}`);
+  }
+
+  writeError(msg: string): void {
+    this.logger.error(`[${this.file} Error] %c${msg}]`, COLORS.red);
+  }
+  writeWarning(msg: string): void {
+    this.logger.warn(`[${this.file} Warning] %c${msg}]`, COLORS.yellow);
+  }
+}
+
+// TODO: Implementar el LoggerAdapter
