82315873229e9d3af2c3a772de0a3a0121809062
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-30 13:11:40 -0600

    fix: Solución del ejercicio del patrón bridge

--- 02-estructurales/02.3-bridge.ts
+++ 02-estructurales/02.3-bridge.ts
@@ -9,3 +9,68 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+import {COLORS} from "../helpers/colors.ts"; // 1. Interfaz NotificationChannel
+
+// 1. Interfaz NotificationChannel
+// Define el método `send`, que cada canal de comunicación implementará.
+interface NotificationChannel {
+  send(message: string): void;
+}
+
+// 2. Implementaciones de Canales de Comunicación
+
+class EmailChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando correo electrónico: ${message}`);
+  }
+}
+
+class SMSChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando SMS: ${message}`);
+  }
+}
+
+class PushNotificationChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando Push: ${message}`);
+  }
+}
+
+// 3. Clase Abstracta Notification
+// Define la propiedad `channel` y el método `notify`
+
+abstract class Notification {
+  constructor(protected channels: NotificationChannel[]) {
+  }
+
+  abstract addChannel(channel: NotificationChannel): void;
+  abstract notify(message: string): void;
+}
+
+class AlertNotification extends Notification {
+  override notify(message: string) {
+    console.log(`\n%cNotificación de Alerta: ${message}`, COLORS.red);
+    this.channels.forEach((channel: NotificationChannel) =>
+      channel.send(message)
+    );
+  }
+
+  override addChannel(channel: NotificationChannel) {
+    this.channels.push(channel);
+  }
+}
+
+function main() {
+  const channels = [
+    new EmailChannel(),
+    new SMSChannel(),
+    new PushNotificationChannel(),
+  ];
+
+  const alert = new AlertNotification(channels);
+
+  alert.notify("Alerta de seguridad: Alguien en frente de la casa");
+}
+
+main();
