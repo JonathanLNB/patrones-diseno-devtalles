23948d56fed4f645765c83bd903aa40745e56ae0
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-30 15:10:58 -0600

    feat: Ejemplo del patrón Proxy

---
+++ 02-estructurales/07-proxy.ts
@@ -0,0 +1,66 @@
+import {COLORS} from "../helpers/colors.ts";
+
+/**
+ * ! Patrón Proxy
+ * Este patrón se utiliza para controlar el acceso a un objeto, es decir,
+ * se crea un objeto que actúa como intermediario entre el cliente y el objeto real.
+ *
+ * * Es útil cuando necesitamos controlar el acceso a un objeto,
+ * * por ejemplo, para verificar si el cliente tiene permiso
+ * * para acceder a ciertos métodos o propiedades.
+ *
+ * https://refactoring.guru/es/design-patterns/proxy
+ */
+class Player {
+  name: string;
+  level: number;
+
+  constructor(name: string, level: number) {
+    this.name = name;
+    this.level = level;
+  }
+}
+
+interface Room {
+  enter(player: Player): void;
+}
+
+class SecretRoom implements Room {
+  enter(player: Player) {
+    console.log(`%cBienvenido a la sala secreta, ${player.name}`, COLORS.blue);
+    console.log("Un gran enemigo te espera");
+  }
+}
+
+class MagicPortal implements Room {
+  private secretRoom: Room;
+
+  constructor(secretRoom: Room) {
+    this.secretRoom = secretRoom;
+  }
+
+  enter(player: Player) {
+    if (player.level >= 10) {
+      this.secretRoom.enter(player);
+      return;
+    }
+    console.log(
+      `%cLo siento mucho ${player.name}, Tu nivel ${player.level} es muy bajo, necesitas nivel 10`,
+      COLORS.red,
+    );
+  }
+}
+
+function main() {
+  const portal = new MagicPortal(new SecretRoom());
+
+  const player1 = new Player("Aventurero A", 5);
+  const player2 = new Player("Aventurero B", 10);
+
+  console.log("%cAventurero A intenta entrar al protal", COLORS.blue);
+
+  portal.enter(player1);
+  portal.enter(player2);
+}
+
+main();
