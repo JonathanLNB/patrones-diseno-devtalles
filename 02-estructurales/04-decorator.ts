395ac9aac9f28ce59da519490ebb52fbc6341368
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-30 14:04:46 -0600

    feat: Ejemplo del patrón Decorator

---
+++ 02-estructurales/04-decorator.ts
@@ -0,0 +1,81 @@
+/**
+ * ! Patrón decorador
+ * Es un patrón de diseño estructural que permite añadir
+ * funcionalidades a objetos, colocando estos objetos dentro de
+ * objetos encapsuladores especiales que contienen estas funcionalidades.
+ *
+ * No confundirlo con los decoradores de TypeScript que son anotaciones.
+ *
+ * * Es útil cuando necesitas añadir funcionalidades a objetos
+ *  * de manera dinámica y flexible.
+ *
+ * https://refactoring.guru/es/design-patterns/decorator
+ */
+import {COLORS} from "../helpers/colors.ts";
+
+interface Notification {
+  send(message: string): void;
+}
+
+class BasicNotification implements Notification {
+  send(message: string): void {
+    console.log(
+      `%cEnviando notificación básica %c${message}`,
+      COLORS.blue,
+      COLORS.white,
+    );
+  }
+}
+
+abstract class NotificactionDecorator implements Notification {
+  protected notification: Notification;
+
+  constructor(notification: Notification) {
+    this.notification = notification;
+  }
+
+  send(message: string): void {
+    this.notification.send(message);
+  }
+}
+
+class EmailDecorator extends NotificactionDecorator {
+  private sendEmail(message: string): void {
+    console.log(
+      `%cEnviando notificación por correo electrónico: %c${message}`,
+      COLORS.green,
+      COLORS.white,
+    );
+  }
+
+  override send(message: string): void {
+    super.send(message);
+    this.sendEmail(message);
+  }
+}
+
+class SMSDecorator extends NotificactionDecorator {
+  private sendSMS(message: string): void {
+    console.log(
+      `%cEnviando notificación por SMS: %c${message}`,
+      COLORS.brown,
+      COLORS.white,
+    );
+  }
+
+  override send(message: string): void {
+    super.send(message);
+    this.sendSMS(message);
+  }
+}
+
+function main() {
+  let notification: Notification = new BasicNotification();
+
+  notification = new EmailDecorator(notification);
+  notification = new SMSDecorator(notification);
+
+  notification.send("Alerta del sistema");
+}
+
+main();
