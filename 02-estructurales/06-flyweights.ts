1512a70cb0b2ce80de728fb9eb6317acf4b4ab98
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-04-30 14:52:55 -0600

    feat: Ejemplo del patrón Flyweight

---
+++ 02-estructurales/06-flyweights.ts
@@ -0,0 +1,82 @@
+/**
+ * ! Patrón Flyweight
+ * Es un patrón de diseño estructural que nos permite usar objetos compartidos
+ * para soportar eficientemente grandes cantidades de objetos.
+ *
+ * * Es útil cuando necesitamos una gran cantidad de objetos y queremos reducir
+ * * la cantidad de memoria que utilizan.
+ *
+ * https://refactoring.guru/es/design-patterns/flyweight
+ */
+import {COLORS} from "../helpers/colors.ts";
+
+interface Location {
+  display(coordinates: { x: number; y: number }): void;
+}
+
+//Flyweight
+class LocationIcon implements Location {
+  private type: string;
+  private iconImage: string;
+
+  constructor(type: string, iconImage: string) {
+    this.type = type;
+    this.iconImage = iconImage;
+  }
+
+  display(coordinates: { x: number; y: number }): void {
+    console.log(
+      `Coords: ${this.type}, en ${coordinates.x}, ${coordinates.y} con ícono %c[${this.iconImage}]`,
+      COLORS.green,
+    );
+  }
+}
+
+// Fabrica de Flyweights
+class LocationFactory {
+  private icons: Record<string, LocationIcon> = {};
+
+  getLocationIcon(type: string): LocationIcon {
+    if (!this.icons[type]) {
+      console.log(
+        `%cCreando una instancia del ícono ${type.toLowerCase()}`,
+        COLORS.brown,
+      );
+      const icon = `image_de_${type.toLowerCase()}.png`;
+      this.icons[type] = new LocationIcon(type, icon);
+    }
+    return this.icons[type];
+  }
+}
+
+class MapLocation {
+  private coordinates: { x: number; y: number };
+  private icon: LocationIcon;
+
+  constructor(x: number, y: number, icon: LocationIcon) {
+    this.coordinates = { x, y };
+    this.icon = icon;
+  }
+
+  display(): void {
+    this.icon.display(this.coordinates);
+  }
+}
+
+function main() {
+  const factory = new LocationFactory();
+
+  const locations = [
+    new MapLocation(10, 20, factory.getLocationIcon("hospital")),
+    new MapLocation(20, 40, factory.getLocationIcon("hospital")),
+    new MapLocation(30, 60, factory.getLocationIcon("hospital")),
+    new MapLocation(35, 45, factory.getLocationIcon("parque")),
+    new MapLocation(5, 20, factory.getLocationIcon("parque")),
+    new MapLocation(80, 20, factory.getLocationIcon("hospital")),
+    new MapLocation(40, 90, factory.getLocationIcon("escuela")),
+  ];
+
+  locations.forEach((location) => location.display());
+}
+
+main();
