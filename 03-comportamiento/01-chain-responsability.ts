bf91680ce5a09cc1c2dd47c91f82bd4a115de63f
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2026-05-02 01:09:38 -0600

    feat: Ejemplo del patrón Cadena de responsabilidad

---
+++ 03-comportamiento/01-chain-responsability.ts
@@ -0,0 +1,75 @@
+/**
+ * ! Patron Chain of Responsibility
+ * Es un patrón de diseño de comportamiento que te permite pasar solicitudes
+ * a lo largo de una cadena de manejadores.
+ *
+ * * Es útil cuando se necesita procesar datos de diferentes maneras, pero no
+ * * se sabe de antemano qué tipo de procesamiento se necesita o en qué orden
+ * * pero se sabe que se necesita procesar en una secuencia.
+ *
+ * https://refactoring.guru/es/design-patterns/chain-of-responsibility
+ */
+import {COLORS} from "../helpers/colors.ts";
+
+interface Handler {
+  setNext(handler: Handler): Handler;
+  handle(request: string): void;
+}
+
+abstract class BaseHandler implements Handler {
+  private nextHandler?: Handler;
+
+  setNext(handler: Handler): Handler {
+    this.nextHandler = handler;
+    return handler;
+  }
+  handle(request: string): void {
+    if (this.nextHandler) {
+      this.nextHandler.handle(request);
+    }
+  }
+}
+
+class BasicSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === "básico") {
+      console.log("%cSoporte básico: Resolvio problema básico", COLORS.green);
+      return;
+    }
+    console.log("Soporte básico: Pasando problema a soporte avanzado");
+    super.handle(request);
+  }
+}
+
+class AdvancedSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === "avanzado") {
+      console.log("%cSoporte avanzado: Resolvio problema avanzado", COLORS.brown);
+      return;
+    }
+    console.log("Soporte avanzado: Pasando problema a soporte experto");
+    super.handle(request);
+  }
+}
+
+class ExpertSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === "experto") {
+      console.log("%cSoporte experto: Resolvio problema experto", COLORS.blue);
+      return;
+    }
+    console.log("Soporte experto: No hay nada que hacer");
+  }
+}
+
+function main() {
+  const basicSupport = new BasicSupport();
+  const advancedSupport = new AdvancedSupport();
+  const expertSupport = new ExpertSupport();
+
+  basicSupport.setNext(advancedSupport).setNext(expertSupport);
+
+  basicSupport.handle("experto");
+}
+
+main();
